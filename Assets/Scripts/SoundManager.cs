cc968572e935d0ad9b609f8241b6e1a714c7386c
zainabhusain227 <41973371+zainabhusain227@users.noreply.github.com>
2024-01-09 17:32:11 -0500

    update for merging

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -57,27 +57,21 @@ public class SoundManager : MonoBehaviour
             //Debug.Log("gridManager.grid[x] " + CVSR.ListofGridsWithAmbient[0].transform.position.x);
             //Debug.Log("gridManager.grid[y] " + CVSR.ListofGridsWithAmbient[0].transform.position.y);
 
-            Vector2 user_pos = new Vector2((float)user_position_row, (float)user_position_col);
+            Vector2 pointA = new Vector2((float)user_position_row, (float)user_position_col);
             //Debug.Log("ambient coords y: " + CVSR.ListofGridsWithAmbient[0][0]);
            // Debug.Log("ambient coords x: " + CVSR.ListofGridsWithAmbient[0][1]);
 
-            Vector2 sound_object_pos = new Vector2((float)myRow, (float)myCol);
+            Vector2 pointB = new Vector2((float)myRow, (float)myCol);
 
             // Calculate distance
-            float distance = CalculateDistance(sound_object_pos,user_pos);
+            float distance = CalculateDistance(pointA, pointB);
             Debug.Log("Distance: " + distance);
             //if distance is <=5 volume=1 to 0.5 , else vol =0
-            if (distance == 0)
-            {   
-                //if you are standing directly on the sound source
-                this.gameObject.GetComponent<AudioSource>().volume = 1;
-                this.gameObject.GetComponent<AudioSource>().panStereo = 0;
-            }
-            else if(distance <= max_distance)
+            if(distance <= max_distance)
             {
                 float volume = 1 - ((1/max_distance) * distance); //slope formula for setting volume, slope=volume/distance=1/5
                 // Calculate angle
-                float angle = CalculateAngle(user_pos, sound_object_pos);
+                float angle = CalculateAngle(pointA, pointB);
                 Debug.Log("Angle: " + angle);
 
                 // Map angle to output
@@ -131,10 +125,10 @@ public class SoundManager : MonoBehaviour
         return Vector2.Distance(pointA, pointB);
     }
 
-    /*float CalculateAngle(Vector2 pointA, Vector2 pointB)
+    float CalculateAngle(Vector2 pointA, Vector2 pointB)
     {
         // Calculate the angle in radians using Atan2
-        float angleRadians = Mathf.Atan2(pointA.x - pointB.x, pointA.y - pointB.y);
+        float angleRadians = Mathf.Atan2(pointB.y - pointA.y, pointB.x - pointA.x);
 
         // Convert radians to degrees
         float angleDegrees = angleRadians * Mathf.Rad2Deg;
@@ -143,26 +137,6 @@ public class SoundManager : MonoBehaviour
         angleDegrees = (angleDegrees + 360) % 360;
 
         return angleDegrees;
-    }*/
-
-    static float CalculateAngle(Vector2 user_pos, Vector2 sound_object_pos)
-    {
-        // Calculate the direction vector from user position to the sound object position
-        Vector2 direction = sound_object_pos - user_pos;
-
-        // Calculate the angle in radians using Math.Atan2  //had to switch coordinated and add a - to the x to make it work? something off about the coordinate system
-        float angleRad = (float)Math.Atan2(-direction.x, direction.y);
-
-        // Convert radians to degrees manually
-        float angleDeg = angleRad * (180.0f / (float)Math.PI);
-
-        // Ensure the angle is positive
-        if (angleDeg < 0)
-        {
-            angleDeg += 360.0f;
-        }
-
-        return angleDeg;
     }
 
     float MapAngleToPanning(float angle)
@@ -173,11 +147,11 @@ public class SoundManager : MonoBehaviour
         {
             panvalue = (90 - angle) / 90;
         }
-        else if (angle > 90 && angle < 180)
+        else if (angle > 90 && angle <= 180)
         {
             panvalue = ((180 - angle) * -1) / 90;
         }
-        else if (angle >= 180 && angle <= 270)
+        else if (angle > 180 && angle <= 270)
         {
             panvalue = ((270 - angle) * -1) / 90;
         }
